c6cae8853e5cc9519820e9af38b5557eae31869c
Gerald Chee <geraldchee@Geralds-MacBook-Air.local>
2024-07-12 21:38:47 +0800

    made join group nicer and changed layout

--- app/(tabs)/joingroup.tsx
+++ app/(tabs)/joingroup.tsx
@@ -5,12 +5,12 @@ import styles from '../../assets/styles';
 import { useNavigation } from '@react-navigation/native';
 import { HeaderBackButton } from '@react-navigation/elements';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
 interface Props {
-  
   navigation: any;
 }
 
-const JoinGroupPage: React.FC<Props> = ({navigation }) => {
+const JoinGroupPage: React.FC<Props> = ({ navigation }) => {
   const [code, setCode] = useState('');
   const [joining, setJoining] = useState(false);
   const [error, setError] = useState(null);
@@ -39,29 +39,37 @@ const JoinGroupPage: React.FC<Props> = ({navigation }) => {
         <Text style={styles.headerText}>Join Group</Text>
       </View>
       
-      <View style={styles.descText}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <TextInput
-          style={styles.inputText}
-          placeholder="Enter 8-digit code"
+          style={{
+            height: 40,
+            backgroundColor: 'black',
+            borderWidth: 1,
+            paddingHorizontal: 10,
+            fontSize: 18,
+            textAlign: 'center',
+            color: 'white'
+          }}
+          
+          placeholder="Click to enter 8-digit code"
+          placeholderTextColor="gray"
           value={code}
           onChangeText={(text) => setCode(text)}
-          
           maxLength={8}
         />
+        <TouchableOpacity
+          style={{
+            backgroundColor: 'purple',
+            padding: 10,
+            borderRadius: 5,
+            marginTop: 20
+          }}
+        >
+          <Text style={{ fontSize: 26, color: "white", textAlign: 'center' }}> SUBMIT </Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity
-            style={{ ...styles.loginButton, backgroundColor: 'purple' }}
-            
-          >
-            <Text style={{ fontSize: 26, color: "white", textAlign: 'center' }}> SUBMIT </Text>
-          </TouchableOpacity>
-    
-
     </SafeAreaView>
   );
 };
-
-
-
 
 export default JoinGroupPage;
