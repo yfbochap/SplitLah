f103c2eea756bd499c36693e64a94d9c0e7ce321
WongYH <wyhui@outlook.com>
2024-07-17 22:05:40 +0800

    group chat designing

--- app/(tabs)/groupchat.tsx
+++ app/(tabs)/groupchat.tsx
@@ -110,25 +110,25 @@ export default function GroupChatScreen(){
 
     return (
 
-        <SafeAreaView style={groupchatStyle.container}>
+        <SafeAreaView style={styles.gc_container}>
             <View style={styles.groupheader}>
                 <View>
                     <HeaderBackButton tintColor='white' onPress={handleBackButtonPress} />
                 </View>
             </View>
-            <View style={groupchatStyle.messageListContainer} >
-                <ScrollView contentContainerStyle={groupchatStyle.scrollViewContent} ref={scrollViewRef} onContentSizeChange={handleAutoScroll}>
+            <View style={styles.gc_messageListContainer} >
+                <ScrollView contentContainerStyle={styles.gc_scrollViewContent} ref={scrollViewRef} onContentSizeChange={handleAutoScroll}>
                     {messages.map((message, index) => (
-                        <View key={index} style={groupchatStyle.messageContainer}>
-                            <Text style={groupchatStyle.receivedName}>{message.user_name} ({message.time_stamp})</Text>
-                            <Text style={groupchatStyle.receivedMessage}>{message.message}</Text>
+                        <View key={index} style={styles.gc_messageContainer}>
+                            <Text style={styles.gc_receivedName}>{message.user_name} ({message.time_stamp})</Text>
+                            <Text style={styles.gc_receivedMessage}>{message.message}</Text>
                         </View>
                     ))}
                 </ScrollView>
             </View>
-            <KeyboardAvoidingView style={groupchatStyle.inputContainer}>
+            <KeyboardAvoidingView style={styles.gc_inputContainer}>
                 <TextInput
-                    style={groupchatStyle.textInput}
+                    style={styles.gc_textInput}
                     value={newMessage}
                     onChangeText={setNewMessage}
                     placeholder="Type a message..."
@@ -138,72 +138,12 @@ export default function GroupChatScreen(){
                     numberOfLines={1}
                 />
                 <TouchableOpacity
-                    style={groupchatStyle.sendButton}
+                    style={styles.gc_sendButton}
                     onPress={handleSendMessage}
                 >
-                    <Text style={groupchatStyle.sendButtonText}>Send</Text>
+                    <Text style={styles.gc_sendButtonText}>Send</Text>
                 </TouchableOpacity>
             </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
-const groupchatStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'black'
-    },
-    messageListContainer: {
-        flex: 0.93,
-        padding: 10,
-    },
-    scrollViewContent: {
-        paddingHorizontal: 10,
-    },
-    messageContainer: {
-        marginVertical: 1,
-        padding: 10,
-        borderRadius: 5,
-        backgroundColor: '#333333',
-    },
-    receivedName: {
-        fontSize: 14,
-        color: '#D3D3D3',
-        marginBottom: 4,
-    },
-    receivedMessage: {
-        fontSize: 20,
-        color: '#E6E6FA',
-    },
-    inputContainer: {
-        flex: 0.07,
-        padding: 10,
-        backgroundColor: '#4c4949',
-        flexDirection: 'row',
-    },
-    textInput: {
-        height: 40,
-        paddingHorizontal: 10,
-        borderRadius: 5,
-        backgroundColor: '#4c4949',
-        flex: .85,
-        flexDirection: 'column',
-        color: '#E6E6FA',
-
-    },
-    sendButton:{
-        height: 40,
-        backgroundColor: '#800080',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        flex: .15,
-        flexDirection: 'column',
-    },
-    sendButtonText:{
-        color: '#fff',
-        fontSize: 16,
-        textAlign: 'center',
-        textAlignVertical:'center'
-    }
-
-});
